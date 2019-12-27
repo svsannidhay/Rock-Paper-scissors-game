4b79a3da28957b3e65544d0a7e9577550d470466
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-12-28 00:17:01 +0530

    Rock-Paper-Scissors-game

---
+++ app.js
@@ -0,0 +1,33 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreboard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getCoputerChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
+
+function game(userChoice){
+    const computerChoice = getCoputerChoice();
+    console.log("user choice = > " + userChoice);
+    console.log("computer choice => " + computerChoice);    
+}
+function main(){
+    rock_div.addEventListener('click',function(){
+        game("r");
+    })
+    paper_div.addEventListener('click',function(){
+        game("p");
+    })
+    scissors_div.addEventListener('click',function(){
+        game("s");
+    })
+}
+main();
